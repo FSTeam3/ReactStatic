b5719312468451aa2beb4c289588e4ff91dc16e7
avinash <avinash.shelke@lntinfotech.com>
2022-10-20 19:24:42 +0530

    Changes made In Form Field

--- src/compo/Forms.js
+++ src/compo/Forms.js
@@ -3,41 +3,34 @@ import React from 'react'
 const Forms = () => {
   return (
     <div className='col-md-8 mx-auto'><div className="mb-3">
-    <label for="exampleFormControlInput1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+    <label for="fname" className="form-label">First Name</label>
+    <input type="text" className="form-control" id="fname" placeholder="Enter First Name Here"/>
   </div>
   <div className="mb-3">
-    <label for="exampleFormControlInput1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+    <label for="lname" className="form-label">Last Name</label>
+    <input type="text" className="form-control" id="lname" placeholder="Enter Last Name Here"/>
   </div>
   <div className="mb-3">
-    <label for="exampleFormControlInput1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+    <label for="email" className="form-label">Email address</label>
+    <input type="email" className="form-control" id="email" placeholder="Enter Email Here"/>
   </div>
   <div className="mb-3">
-    <label for="exampleFormControlInput1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+    <label for="address" className="form-label">Address</label>
+    <input type="email" className="form-control" id="address" placeholder="Enter Address Here"/>
   </div>
   <div className="mb-3">
-    <label for="exampleFormControlInput1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+    <label for="vehnumber" className="form-label">Vehicle Number</label>
+    <input type="text" className="form-control" id="vehnumber" placeholder="Enter Vehicle Number Here"/>
   </div>
   <div className="mb-3">
-    <label for="exampleFormControlInput1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+    <label for="engineNumber" className="form-label">Engine Number</label>
+    <input type="text" className="form-control" id="engineNumber" placeholder="Enter Engine Number Here"/>
   </div>
   <div className="mb-3">
-    <label for="exampleFormControlInput1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+    <label for="vehType" className="form-label">Vehicle Type</label>
+    <input type="text" className="form-control" id="vehType" placeholder="Enter Vehicle Type Here"/>
   </div>
-  <div className="mb-3">
-    <label for="exampleFormControlInput1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
   </div>
-  <div className="mb-3">
-    <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-  </div></div>
   )
 }
 
