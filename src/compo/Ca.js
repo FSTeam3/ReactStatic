e2022b499f80dafac4a05c30aa26951c7b8f4cb0
avinash <avinash.shelke@lntinfotech.com>
2022-12-07 23:40:31 +0530

    ca component img added

--- src/compo/Ca.js
+++ src/compo/Ca.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import Card_data from '../datas/Card_data'
 import { Outlet, Link } from "react-router-dom";
+import car from './Insurance_Car.jpg'
+
 const Ca = () => {
   return (
-    <div class="container">
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+    <div className="container">
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
         {Card_data.map((data) => {
-          return <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-              <div class="card-body">
-                <p class="card-text">{data.p}</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary"><Link to="/forms">{data.btn1}</Link></button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">{data.btn2}</button>
+          return <div className="col">
+            <div className="card shadow-sm">
+            <img className="bd-placeholder-img card-img-top" width="100%" height="225" src={car} xmlns={car} role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
+              <div className="card-body">
+                <p className="card-text">{data.p}</p>
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="btn-group">
+                    <button type="button" className="btn btn-sm btn-outline-secondary"><Link to="/forms">{data.btn1}</Link></button>
+                    <button type="button" className="btn btn-sm btn-outline-secondary">{data.btn2}</button>
                   </div>
-                  <small class="text-muted">{data.min}</small>
+                  <small className="text-muted">{data.min}</small>
                 </div>
               </div>
             </div>
