0194e9d60b614961283c92b5b95333bb389c9366
avinash <avinash.shelke@lntinfotech.com>
2022-12-18 01:37:35 +0530

    form modified

--- src/compo/Forms.js
+++ src/compo/Forms.js
@@ -1,39 +1,116 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Outlet, Link } from "react-router-dom";
-const Forms = () => {
-  return (
-    <div className='col-md-8 mx-auto mt-2 border border-round'><div className="mb-3 mx-5 mt-2">
-    <label for="fname" className="form-label">First Name</label>
-    <input type="text" className="form-control" id="fname" placeholder="Enter First Name Here" required autofocus/>
-  </div>
-  <div className="mb-3 mx-5 mt-2">
-    <label for="lname" className="form-label">Last Name</label>
-    <input type="text" className="form-control" id="lname" placeholder="Enter Last Name Here" required/>
-  </div>
-  <div className="mb-3 mx-5 mt-2">
-    <label for="email" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="email" placeholder="Enter Email Here" required/>
-  </div>
-  <div className="mb-3 mx-5 mt-2">
-    <label for="address" className="form-label">Address</label>
-    <input type="email" className="form-control" id="address" placeholder="Enter Address Here" required/>
-  </div>
-  <div className="mb-3 mx-5 mt-2">
-    <label for="vehnumber" className="form-label">Vehicle Number</label>
-    <input type="text" className="form-control" id="vehnumber" placeholder="Enter Vehicle Number Here"/>
-  </div>
-  <div className="mb-3 mx-5 mt-2">
-    <label for="engineNumber" className="form-label">Engine Number</label>
-    <input type="text" className="form-control" id="engineNumber" placeholder="Enter Engine Number Here"/>
-  </div>
-  <div className="mb-3 mx-5 mt-2">
-    <label for="vehType" className="form-label">Vehicle Type</label>
-    <input type="text" className="form-control" id="vehType" placeholder="Enter Vehicle Type Here"/>
-  </div>
-  <button className='btn btn-primary mb-3 mx-5 mt-2'><Link className="nav-link text-light" to="/pricing">Submit</Link></button>
-  < Outlet/>
-  </div>
-  )
-}
+// const Forms = () => {
+//   return (
+//   <div></div>
+//   //   <div className='col-md-8 mx-auto mt-2 border border-round'>
+{/* <div className="mb-3 mx-5 mt-2"> */ }
+//   //   <label for="fname" className="form-label">First Name</label>
+//   //   <input type="text" className="form-control" id="fname" placeholder="Enter First Name Here" required autofocus/>
+//   // </div>
+//   // <div className="mb-3 mx-5 mt-2">
+//   //   <label for="lname" className="form-label">Last Name</label>
+//   //   <input type="text" className="form-control" id="lname" placeholder="Enter Last Name Here" required/>
+//   // </div>
+//   // <div className="mb-3 mx-5 mt-2">
+//   //   <label for="email" className="form-label">Email address</label>
+//   //   <input type="email" className="form-control" id="email" placeholder="Enter Email Here" required/>
+//   // </div>
+//   // <div className="mb-3 mx-5 mt-2">
+//   //   <label for="address" className="form-label">Address</label>
+//   //   <input type="email" className="form-control" id="address" placeholder="Enter Address Here" required/>
+//   // </div>
+//   // <div className="mb-3 mx-5 mt-2">
+//   //   <label for="vehnumber" className="form-label">Vehicle Number</label>
+//   //   <input type="text" className="form-control" id="vehnumber" placeholder="Enter Vehicle Number Here"/>
+//   // </div>
+//   // <div className="mb-3 mx-5 mt-2">
+//   //   <label for="engineNumber" className="form-label">Engine Number</label>
+//   //   <input type="text" className="form-control" id="engineNumber" placeholder="Enter Engine Number Here"/>
+//   // </div>
+//   // <div className="mb-3 mx-5 mt-2">
+//   //   <label for="vehType" className="form-label">Vehicle Type</label>
+//   //   <input type="text" className="form-control" id="vehType" placeholder="Enter Vehicle Type Here"/>
+//   // </div>
+//   // <button className='btn btn-primary mb-3 mx-5 mt-2'><Link className="nav-link text-light" to="/pricing">Submit</Link></button>
+//   // < Outlet/>
+//   // </div>
 
-export default Forms
+//   )
+// }
+
+// export default Forms
+
+
+function Form() {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [number, setNumber] = useState('')
+  const [rcno, setRcno] = useState('')
+
+  const [error, setError] = useState(false)
+  const [proceed, isProceed] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (firstName.length == 0 || lastName.length == 0) {
+      setError(true)
+    }
+    // if (firstName && lastName && number && rcno && email) {
+    //   // console.log("First Name: ", firstName, "\nLast Name: ", lastName)
+    //   isProceed(true);
+    // }
+    // console.log(proceed);
+  }
+
+  const checkSubmitButtom = () =>{
+    if (firstName && lastName && number && rcno && email) {
+      // console.log("First Name: ", firstName, "\nLast Name: ", lastName)
+      isProceed(true);
+    }
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit} className='col-md-8 mx-auto mt-2 border border-round'>
+        <div className="mb-3 mx-5 mt-2">
+          <label for="fname" className="form-label">First Name</label>
+          <input placeholder="First Name" className='form-control' onBlur={checkSubmitButtom} on onChange={e => setFirstName(e.target.value)} />
+          {error && firstName.length <= 0 ?
+            <label  className='text-danger'>First Name can't be Empty</label> : ""}</div>
+
+
+        <div className="mb-3 mx-5 mt-2">
+          <label for="lname" className="form-label">Last Name</label>
+          <input placeholder="Last Name" className='form-control' onBlur={checkSubmitButtom} onChange={e => setLastName(e.target.value)} />
+          {error && lastName.length <= 0 ?
+            <label className='text-danger'>Last Name can't be Empty</label> : ""}</div>
+
+
+            <div className="mb-3 mx-5 mt-2">
+          <label for="email" className="form-label">Email</label>
+          <input placeholder="Email" type="email" className='form-control' onBlur={checkSubmitButtom} onChange={e => setEmail(e.target.value)} />
+          {error && email.length <= 10 ?
+            <label className='text-danger'>Email can't be Empty</label> : ""}</div>
+
+            <div className="mb-3 mx-5 mt-2">
+          <label for="fname" className="form-label">Contact No</label>
+          <input placeholder="Contact Number" className='form-control' onBlur={checkSubmitButtom}  onChange={e => setNumber(e.target.value)} />
+          {error && (number.length  <= 0 && number.length > 10 ) ?
+            <label className='text-danger'>Contact number should be 10 digits</label> : ""}</div>
+            <div className="mb-3 mx-5 mt-2">
+          <label for="fname" className="form-label">RC No</label>
+
+          <input placeholder="RC no" className='form-control' onBlur={checkSubmitButtom} onChange={e => setRcno(e.target.value)} />
+          {error && rcno.length <= 0 ?
+            <label className='text-danger'>RC NO can't be Empty</label> : ""}</div>
+        <div>
+          {proceed && <button className='btn btn-primary mx-5 mb-3' type='hidden'>
+            <Link className="nav-link text-light" to="/pricing">Submit</Link>
+          </button>}
+        </div>
+      </form>
+    </>
+  );
+}
+export default Form;
